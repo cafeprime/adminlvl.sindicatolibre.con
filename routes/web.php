56312a313ac652c3ce5f67ecd66350ec80732f0d
Cafeprime <cafe.prime@gmail.com>
2024-09-23 18:00:48 +0200

    Pruebas con vite

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('login');
-});
+Route::get('/', [LoginController::class, 'login']);
+
+// Así nombramos rutas
+// Route::get('/url1/{var}', function () {
+//     return route('url.quesea');
+// })->where('var', '[A-Za-z]'); //Protegemos el parametro var permitiendo sólo letras
+// Route::get('/urlquesea', function(){
+//     return "Esta es la ruta que sea";
+// })->name('url.quesea');
