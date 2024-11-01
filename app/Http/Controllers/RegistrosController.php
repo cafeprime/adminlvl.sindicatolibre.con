3c98041ee0421f662e2f44c9b84ebf33bb3f5d6a
Cafeprime <cafe.prime@gmail.com>
2024-11-01 23:00:24 +0100

    Listado de registros con buscador OK

--- app/Http/Controllers/RegistrosController.php
+++ app/Http/Controllers/RegistrosController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\Registro;
 use App\Models\Curso;
-use Illuminate\Http\Request;
 use App\Models\Provincia;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class RegistrosController extends Controller
 {
-    public function index(Request $request){
+    public function index(Request $request)
+    {
 
         // Este IF cambia la variable page por p para seleccionar página en la URL como en mi antigua App
         if ($request->has('p')) {
@@ -17,17 +19,26 @@ class RegistrosController extends Controller
             $request->query->remove('page'); // Elimina el parámetro `page`
             $request->merge(['page' => $page]); // Fusiona `p` como `page`
         }
-
-        $registros = Registro::orderBy('FECHA_INSCRIPCION', 'desc')->paginate(50);
-        $totalRegistros = Registro::count();
+        
+        // Consulta de provincias y cursos
+        $provincias = Provincia::orderBy('PROVINCIA', 'asc')->get();
         $cursos = Curso::all(); // Obtiene todos los cursos
-        $provincias = Provincia::orderBy('PROVINCIA', 'asc')->get(); // Obtiene provincias ordenadas
+
+        // Resto de lógica y paginación. AQUI se accede a las variables de la url
+        $limit = $request->input('f', 50);
+        $provinciaId = $request->input('v', null); // ID de provincia desde el parámetro `v`
+        $tipo = $request->input('t', 0);
+        $roleId = Auth::user()->role_name;
+
+        // Consulta de registros usando el método en el modelo
+        $registros = Registro::filtrarRegistros($roleId, $provinciaId, $tipo)->paginate($limit);
 
         return view('registros.listar', [
             'registros' => $registros,
-            'totalRegistros' => $totalRegistros,
-            'cursos' => $cursos,
-            'provincias' => $provincias,
+            'totalRegistros' => Registro::filtrarRegistros($roleId, $provinciaId, $tipo)->count(),
+            'provincias' => $provincias, // Pasar las provincias a la vista
+            'provinciaId' => $provinciaId, // Provincia seleccionada
+            'cursos' => $cursos
         ]);
     }
     
